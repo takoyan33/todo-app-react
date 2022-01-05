09a2da1d14bbb474d64fe034dce94d007d9034f3
abe shmupei <harrier2070@gmail.com>
2022-01-06 08:05:33 +0900

    codefix

--- src/components/CompleteTodos.js
+++ src/components/CompleteTodos.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
- export const CompleteTodos = (props:props)=> {
+export const CompleteTodos = (props: props) => {
+  const { todos, onClickBack } = props;
 
-const { todos, onClickBack } = props;
-
-  return(
-    <Stack  direction="row" spacing={2}  className="complete-area">
-    <p className="text-1xl font-bold">
-    完了のTODO
-    </p>
-    <ul>
-    {todos.map((todo, index) => {
-      return(
-        <div key={todo} className="list-row">
-          <p>{ todo } </p>
-            <Button variant="contained" onClick={()=> onClickBack(index)}>戻す</Button>
-      </div>
-      );
-})}
-    </ul>
-  </Stack>
+  return (
+    <Stack direction="row" spacing={2} className="complete-area">
+      <p className="text-1xl font-bold">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <p>{todo} </p>
+              <Button variant="contained" onClick={() => onClickBack(index)}>
+                戻す
+              </Button>
+            </div>
+          );
+        })}
+      </ul>
+    </Stack>
   );
 };
